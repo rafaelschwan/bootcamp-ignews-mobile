7947cd9743411bfa404acffa0b30b279df7e6e49
Rafael Schwan <rafaelschwan@hotmail.com>
2023-07-05 16:32:40 -0300

    Construida tela de signin e signup

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/
 import { THEME } from './src/theme';
 
 import { Loading } from '@components/Loading';
+import { SignUp } from '@screens/SignUp';
 
 export default function App() {
 
@@ -19,7 +20,7 @@ export default function App() {
         translucent
       />
 
-        {fontLoaded ? <View /> : <Loading />}
+        {fontLoaded ? <SignUp /> : <Loading />}
 
     </NativeBaseProvider>
   );
